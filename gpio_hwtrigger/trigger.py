6f5ec7e5ea2e70dff9a449ac84fed9c8e3befaab
pabu <patrick.burger@unibw.de>
2016-10-26 15:53:55 +0200

    gpio_hwtrigger: add wiring pinout, add some prints

--- gpio_hwtrigger/trigger.py
+++ gpio_hwtrigger/trigger.py
@@ -2,7 +2,7 @@ __author__ = 'patrick@codegrafix.de'
 # !/usr/bin/python
 import sys
 from time import sleep
-
+import time
 import wiringpi
 
 on = 1
@@ -10,45 +10,43 @@ off = 0
 
 class raspiCamHWTrigger():
 
-    hz = float(sys.argv[1])
-    pulselength = float(1/hz/2)  # seconds
-    print(pulselength)
-
     # HW pin (11,12,13,15)
+    # http://wiringpi.com/wp-content/uploads/2013/03/gpio1.png
+    # left center right
     pins=[0,1,2,3]
+
+
+    hz = float(sys.argv[1])
+    trigger_delta=float(1/hz/len(pins))
+    pulse_length = float(trigger_delta*3/4)  # seconds
+    print("Hz: %d - TriggerDelta: %f -  Pulselength: %f" % (hz,trigger_delta,pulse_length))
 
     def __init__(self):
         wiringpi.wiringPiSetup()
         # Set pins as output
         for pin in self.pins:
-            print("Set pin to %d", pin)
+            #print("Set pin %d as output", pin)
             wiringpi.pinMode(pin, 1)
 
-    def trigger(self, number_of_high_pulses, number_of_low_pulses):
-
-        wiringpi.digitalWrite(self.pins[0], on)
-        sleep(self.pulselength * number_of_high_pulses)
-        wiringpi.digitalWrite(self.pins[0], off)
-
-        wiringpi.digitalWrite(self.pins[1], on)
-        sleep(self.pulselength * number_of_low_pulses)
-        wiringpi.digitalWrite(self.pins[1], off)
-
-        wiringpi.digitalWrite(self.pins[2], on)
-        sleep(self.pulselength * number_of_high_pulses)
-        wiringpi.digitalWrite(self.pins[2], off)
-
-        wiringpi.digitalWrite(self.pins[3], on)
-        sleep(self.pulselength * number_of_low_pulses)
-        wiringpi.digitalWrite(self.pins[3], off)
+    def trigger(self):
+        for pin in self.pins:
+            wiringpi.digitalWrite(pin, on)
+            #print("pin: %d ON" % pin)
+            sleep(self.pulse_length)
+            #print("pin: %d OFF after %f" % (pin, self.pulse_length))
+            wiringpi.digitalWrite(pin, off)
+            sleep(self.trigger_delta-self.pulse_length)
+            #print("Sleeping for %f" % (self.trigger_delta-self.pulse_length))
 
 if __name__ == '__main__':
 
     trigger = raspiCamHWTrigger()
-    print("Trigger time for all cameras: %d Hz"  %(trigger.hz) )
 
     while 1:
-        trigger.trigger( 1, 1)
-        #sleep(self.pulselength)
-        #trigger.trigger(1, 1, 1)
-        #sleep(self.pulselength)
+        start = time.time()
+        trigger.trigger()
+        elapsed = time.time() - start
+        print("hz count: %f" % ( 1/elapsed))
+
+
+
